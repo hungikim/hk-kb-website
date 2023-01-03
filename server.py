f7fd7cb760529d5e8bf9a44afedb462128775664
Ryukibum11 <100761196+Ryukibum11@users.noreply.github.com>
2023-01-03 22:30:56 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask # 연습용
+from flask import Flask #연습용
 import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return str(random.random())
+   return 'welcome'
+
+@app.route('/create/')
+def create():
+   return 'Create'
+
+@app.route('/read/1/')
+def read():
+   return 'Read 1'
 
 app.run(port = 5001, debug = True)
